c140427c0c2bf41331079d48f8b6246823f30962
ChiAn <ChiAn_Ate@Outlook.com>
2022-11-12 15:29:46 +0800

    函桉修改了文件.

---
+++ Content/Tool.js
@@ -0,0 +1,34 @@
+/*
+
+
+CI:{
+    Title:'聚焦文学美:麓馆文学殿堂',
+        ConciseIntroduce:'享誉盛名，这座深藏在树林深处的仿古建筑是首都文学界的骄傲。',
+        Url:{
+        Name:'Point_JadeGarden_Hall_Ru',
+            Introduce:'',
+            Date:'2022/10/14',
+            OriginalWriter:'成安柚子',
+            Main:[
+            {type:'Main', Content:'享誉盛名，这座深藏在树林深处的仿古建筑是首都文学界的骄傲。',},
+            {type:'Main', Content:'当游客徘徊于枝叶招展的麓园深处，时常会被一座古雅的暗色红砖建筑所震撼。那便是被视为“文学圣地”的麓馆文学殿堂，全港市文部厅机构所谓“首都文联”的总部所在地。',},
+            {type: 'br'},
+            {type: 'Audio',Content: '<iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="150" style="width:100%;max-width:660px;overflow:hidden;background:transparent;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/cn/album/energetic/1569282309?i=1569282316"></iframe>'},
+            {type: 'br'},
+            {type: 'Main',Content: '关于翡翠园：'},
+            {type: 'Main',Content: '翡翠园又名“麓园(Jade Garden)”，位于江代地区東南部，是一座跨时代后建造的仿旧时代式园林，有着“深色碧珠”一称。关于翡翠园内暗色系且古今混搭的清透碧绿摆设与装点设计，在建筑圈内颇有盛名，有人说设计师成功的将古代文化融入了现代的视觉引导布局，也有人说翡翠园的布局集合了“東西方建筑之特色”，给予人极其舒适的视觉体验。该园林与周围街道从卫星上俯瞰犹如一颗镶嵌在都市之中的翡翠明珠，故称为“翡翠园”。翡翠园同时也是“首都文联”的象征，被首都范围的文学人士称为“文学界的精神圣地”，在该街区坐落着密集的“首都文联”办公与学术部门，因此，有时人们会直接用“翡翠园”三字来称呼“首都文联”。'},
+            {type: 'br'},
+            {type:'Audio',Content: '<iframe allow="autoplay *; encrypted-media *;" frameborder="0" height="150" style="width:100%;max-width:660px;overflow:hidden;background:transparent;" sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation" src="https://embed.music.apple.com/cn/album/light/1570736770?i=1570736772"></iframe>'},
+            {type: 'br'},
+            {type: 'Main',Content: '关于首都文联(首都文科学界联合会)：'},
+            {type: 'Main',Content: '首都文联是首都公式旗下的“文科学界”盟会式机构，该机构负责统筹首都范围全部的文科学界发展与前沿学术议论会等。人们对首都文联的评价往往是“由一群陈腐的老年人组成的派对”，为了消除人们的刻板印象，现代第三阶段，首都文联正式成立“对外宣传部门”（即“首都文创中心”），并深刻展开检讨，成立并成建有关学风治理的监督部门。首都文联总部位于江代地区的翡翠园街头，首要的学术报告厅被称为“麓馆会议中心”，因为街道两旁生长着十分古老的法国梧桐，所以这里看起来常年阴暗。" 虽然这群自大的老年人面对新生事物深闭固拒，但对于坚持某些敏感问题的底线，他们却能展现出令人佩服的恪守不渝。"'},
+            {type: 'br'},
+            {type:'Hashtag',Content: ['京川新闻厅-云游全国','成安柚子记者','麓馆文学殿堂','首都文联']},
+        ],
+            Hashtag:'',
+    },
+    For:'收录在《未来述白》',
+}
+
+
+*/
